d837f95a7f8345b965f971e26c6b853e42a6a236
mattlaguardia <matthewlaguardia@gmail.com>
2016-06-26 16:46:18 -0700

    saves work done on auth branch

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -9,6 +9,8 @@ class ApplicationController < ActionController::Base
     def current_user
       if session[:user_id]
         @current_user ||= User.find(session[:user_id])
+      else
+        nil
       end
     end
 
--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,2 +1,22 @@
 class SessionsController < ApplicationController
+  include SessionsHelper
+
+  def new
+  end
+
+  def create
+    user = User.find_by_email(params[:email])
+    if user && user.authenticate(params[:password])
+      session[:user_id] = user.id
+      redirect_to user
+    else
+      redirect_to "/index", flash: { error: "There was a problem logging you in."}
+    end
+  end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
 end
