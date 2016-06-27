01778eb6f144a0d97fc0ea2d0cf2869a810f1a06
mattlaguardia <matthewlaguardia@gmail.com>
2016-06-26 23:34:47 -0700

    adds styling to input box

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+User.destroy_all
+
+users = User.create([{username: "MichaelChengalo", first_name: "Mike", last_name: "Cheng", email: "mc@gmail.com", password_digest: "password1"},
+  {username: "Sullymander", first_name: "Matt", last_name: "Sullivan", email: "ms@gmail.com", password_digest:"password2"},
+  {username: "BenTakesPicOfHisFood", first_name: "Matt", last_name: "Laguardia", email: "ml@gmail.com", password_digest: "password3"},
+  {username: "aJENda", first_name: "Jen", last_name: "Mcphail", email: "jm@gmail.com", password_digest: "password4"}])
