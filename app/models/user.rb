3b225ab9436e70b2c0ca0ccf48ae63eb86257825
Michael Cheng <mcheng.oc@gmail.com>
2016-06-26 17:32:34 -0700

    adds seed data for users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User < ActiveRecord::Base
-	serialize :name, :location, :phone_numbers, :featured_image, :thumb_url, :price_range, :cuisines, :aggregrate_rating, JSON
+	# serialize :name, :location, :phone_numbers, :featured_image, :thumb_url, :price_range, :cuisines, :aggregrate_rating, JSON
 
 	has_many :restaurants
 
