6c7ff987cb6f60cd17d066b500dbbf1e59aa0ce9
jenmcphail <mcphail.jen@gmail.com>
2016-06-27 11:32:14 -0700

    adds user / restaurant association

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,6 @@
 class Restaurant < ActiveRecord::Base
 	
-	serialize :name, :location, :phone_numbers, :featured_image, :thumb_url, :price_range, :cuisines, :aggregrate_rating, JSON
+	# serialize :name, :location, :phone_numbers, :featured_image, :thumb_url, :price_range, :cuisines, :aggregrate_rating, JSON
 
 	has_many :restaurant_users
 	has_many :users, through: :restaurant_users
