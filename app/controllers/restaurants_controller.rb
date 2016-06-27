0172f9ea99ae02b0405e7f4a14e1c532f2a53105
jenmcphail <mcphail.jen@gmail.com>
2016-06-27 12:22:28 -0700

    resolves layout views conflict, adds basic restaurant views.

--- app/controllers/restaurants_controller.rb
+++ app/controllers/restaurants_controller.rb
@@ -1,8 +1,9 @@
 class RestaurantsController < ApplicationController
 
   def index
-    @restaurant = Restaurant.find(params[:id])
+    @restaurant = Restaurant.all
     render :index
+    #index will calls all restaurants on the backend, the view will display them one at a time.
   end
   def show
     @restaurant = Restaurant.find(params[:id])
