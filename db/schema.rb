22372af67364118340b61dc274f56ad740f35466
Michael Cheng <mcheng.oc@gmail.com>
2016-06-27 21:39:53 -0700

    add birthday to users model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160627173611) do
+ActiveRecord::Schema.define(version: 20160628043615) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -45,6 +45,7 @@ ActiveRecord::Schema.define(version: 20160627173611) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.integer  "restaurant_id"
+    t.datetime "birthday"
   end
 
   add_foreign_key "restaurant_users", "restaurants"
