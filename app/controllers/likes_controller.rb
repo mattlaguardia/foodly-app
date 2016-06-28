1bc65669720cc91a32610623107bc754effdfd9b
mattlaguardia <matthewlaguardia@gmail.com>
2016-06-28 15:12:22 -0700

    finishs functionality of likes and dislikes

--- app/controllers/likes_controller.rb
+++ app/controllers/likes_controller.rb
@@ -2,7 +2,7 @@ class LikesController < ApplicationController
 	def create
 		user = User.find(params[:user_id])
 		@restaurant = Restaurant.find(params[:restaurant_id])
-		user.likes.push(@restaurant)
+		user.restaurant_likes.push(@restaurant)
 		redirect_to restaurants_path
 	end
 end
