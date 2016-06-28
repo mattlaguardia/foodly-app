d4e4f53c4e730f0236c110e99f2818d6213e87ee
Michael Cheng <mcheng.oc@gmail.com>
2016-06-28 15:20:21 -0700

    adds users create test

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,6 +1,11 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
+
+  before do
+    @user = User.new(username: "ohhheyimben", first_name: "Ben", last_name: "Yang", email: "ohhheyimben@iamben.com", password: "interBENtion")
+  end
+
   describe "Create" do
     subject(:user) { FactoryGirl.create(:user) }
 
@@ -12,13 +17,33 @@ RSpec.describe User, type: :model do
       expect(user.username).to_not be_nil
     end
 
+    it "should have a first name" do
+      expect(user.first_name).to_not be_nil
+    end
+
+    it "should have a last name" do
+      expect(user.last_name).to_not be_nil
+    end
+
     it "should have a valid email" do
       FactoryGirl.build(:user, :email => "EmailWithoutTheAtSign").should_not be_valid
     end
 
-  describe "New" do
-    it "should direct to users#new page" do
-      expect(3).to eq(3)
+    it "should encrypt user password" do
+      expect(@user.password_digest).to_not eq("interBENtion")
     end
+  end
+
+  describe "when email address is already taken" do
+    before do
+      @test_email = FactoryGirl.build(:user, :email => "ohhheyimben@iamben.com")
+    end
+
+    it "should have a unique email" do
+      user_with_same_email = @user.dup
+      # user_with_same_email.save!
+      expect(@test_email).not_to be_valid
+    end
+
   end
 end
