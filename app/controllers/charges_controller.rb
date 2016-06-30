05aca116cdceb00b5bd50667825a72abd4511426
jenmcphail <mcphail.jen@gmail.com>
2016-06-29 19:36:22 -0700

    adds stripe forms and functionality

--- app/controllers/charges_controller.rb
+++ app/controllers/charges_controller.rb
@@ -4,12 +4,12 @@ class ChargesController < ApplicationController
 
 	def create
 		@amount = 50
-		customer = Stripe::User.create(
+		customer = Stripe::Customer.create(
 			:email => params[:stripeEmail],
 			:source  => params[:stripeToken]
 			)
 		charge = Stripe::Charge.create(
-			:user    => user.id,
+			:customer    => customer.id,
     		:amount      => @amount,
     		:description => 'Rails Stripe customer',
     		:currency    => 'usd'
@@ -19,5 +19,9 @@ class ChargesController < ApplicationController
   			flash[:error] = e.message
   			redirect_to new_charge_path
 		end
+		user = User.find(params[:user_id])
+		customer = Stripe::Customer.find(params[:id])
+		# push customer id into user
+		# if customer id is not nil, show specific views
 	end
 
