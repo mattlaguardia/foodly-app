79515de9fd71f4bad04cfba2be022af5217cfc97
Matt Laguardia <matthewlaguardia@gmail.com>
2016-07-06 13:41:07 -0700

    Delete yelp.rb

--- config/initializers/yelp.rb
+++
@@ -1,8 +0,0 @@
-# require 'yelp'
-#
-# Yelp.client.configure do |config|
-#   config.consumer_key = 'uF1UI9OZxij161yg3mhRdQ',
-#   config.consumer_secret = 'yKNlfYlIBM91wAfmQuhceN2FOIU',
-#   config.token = 'MswYR5gpWd-Gh2QScGxeQr0IvCC-eyfq',
-#   config.token_secret = 'sHoCt6uOOyxtGa0-GE9JUqPtNR0'
-# end
